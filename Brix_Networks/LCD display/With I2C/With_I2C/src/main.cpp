3ddc34cc9172de7b6685e82e109b435803f1a8a8
nithilan0710 <nithilan006@gmail.com>
2025-03-09 13:38:42 +0530

    new commit

--- Brix_Networks/LCD display/With I2C/With_I2C/src/main.cpp
+++ Brix_Networks/LCD display/With I2C/With_I2C/src/main.cpp
@@ -1,19 +1,20 @@
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x3F for a 16 chars (coloumn) and 2 line (row) display
+LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars (coloumn) and 2 line (row) display
 
 void setup() {
   lcd.init();
   lcd.clear();         
   lcd.backlight();      // Make sure backlight is on
-  
-  // Print a message on both lines of the LCD.
-  lcd.setCursor(0,1);   //Set cursor to character 2 on line 0
-  lcd.print("0123456789101112");
-  
-  lcd.setCursor(0,3);   //Move cursor to character 2 on line 1
-  lcd.print("ABCDEFGHIJKLMNOP");
 }
 
 void loop() {
+    // Print a message on both lines of the LCD.
+    lcd.setCursor(0,0);   //Set cursor to character 2 on line 0
+    lcd.print("0123456789101112");
+    
+    lcd.setCursor(0,1);   //Move cursor to character 2 on line 1
+    lcd.print("abcedfghijklmnop");
+    
+    //lcd.print((char)223); //Print degree symbol
 }
